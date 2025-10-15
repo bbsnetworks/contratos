d8b743a8a3a9339243541faf7e24156f0115a67c
bbsnetworks <bbsnetworks0@gmail.com>
2025-10-15 14:09:25 -0600

    cambios integriti

--- lista/index.php
+++ lista/index.php
@@ -7,8 +7,7 @@
   <title>Contratos BBS</title>
   <script src="../js/jspdf.min.js"></script>
   <script src="../js/signature_pad.umd.min.js"></script>
-  <link href="../css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="../css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/bootstrap-icons.min.css">
   <link rel="stylesheet" href="../css/generales.css">
   <link rel="stylesheet" href="../css/index.css">
@@ -83,9 +82,7 @@ if (!isset($_SESSION['username'])) {
     </div>
   </div>
 
-  <script src="../js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
+  <script src="../js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <script src="https://momentjs.com/downloads/moment.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
